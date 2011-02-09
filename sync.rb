709785787c00f2dc408910eea48225b3f88e72e3
newdongyuwei <newdongyuwei@localhost>
2011-02-09 09:16:56 +0000

    reuse the ssh connection

--- sync.rb
+++ sync.rb
@@ -8,27 +8,23 @@ require 'ruby-growl'if RUBY_PLATFORM.downcase.include?("darwin") #only macruby i
 host = "10.210.74.63"
 username = "my name"
 password = "my password"
+scp = Net::SCP.start(host, username, :password => password) # reuse the connection
 
 source_dir = '/Users/yuwei/workspace/miniblog'
 target_dir = '/data1/wwwroot/js.wcdn.cn/dev_js/miniblog'
 
-def upload(host, username,password,source, target)
-    Net::SCP.start(host, username, :password => password) do |scp|
-        scp.upload!(source, target,:recursive => false)do |ch, name, sent, total|
-         #progress reports
-         #puts "#{name}: #{sent}/#{total}"
-        end
-        p "#{source} modified,synced to server !"
-        begin
-            #config growl listen to network connection
-            if Object.const_defined? :Growl
-              g = Growl.new "127.0.0.1", "ruby-growl", ["ruby-growl Notification"]
-              g.notify "ruby-growl Notification", "File sync", "#{source} modified,synced to server !"
-            end
-        rescue
-            p $!
-        end
+def upload(scp, source, target)
+  scp.upload!(source, target,:recursive => false)
+  p "#{source} modified,synced to server !"
+  begin
+   #config growl listen to network connection
+    if Object.const_defined? :Growl
+      g = Growl.new "127.0.0.1", "ruby-growl", ["ruby-growl Notification"]
+      g.notify "ruby-growl Notification", "File sync", "#{source} modified,synced to server !"
     end
+  rescue
+    p $!
+  end
 end
 
 if File.exist?('.lock4autosyn')
@@ -47,10 +43,8 @@ stream = FSEvents::Stream.watch(source_dir) do |events|
     events.each do |event|
         event.modified_files.each do|modified|
             if File.file? modified and not modified.include? '.svn' and not modified.include? '.hg' and not modified.include? '.git'
-                #p modified
                 target  = target_dir + modified.split(source_dir)[1]
-                #p target
-                upload(host, username,password,modified, target)
+                upload(scp,modified, target)
             end
         end
     end
